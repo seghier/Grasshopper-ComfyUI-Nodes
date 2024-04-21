aa702418d26348f7c8a0f7ce19e6784d0017fdac
seghier <seghierdesign@gmail.com>
2024-04-21 12:43:48 +0100

    Update
    Load Image UserObject added

--- Examples/Image2Image/image2image.py
+++ Examples/Image2Image/image2image.py
@@ -6,66 +6,66 @@ import sys
 from urllib import request
         
 def parse_arguments(args):
-    GHPrompt_positive = ""
-    GHFile_imagesketch = ""
-    GHFile_imagedepth = ""
-    GHFile_imagestyle = ""
-    GHInteger_batch = ""
     GHString_model = ""
-    GHString_clipvision = ""
-    GHString_contolnetsketch = ""
     GHString_contolnetdepth = ""
     GHString_stylemodel = ""
+    GHString_contolnetsketch = ""
+    GHString_clipvision = ""
+    GHString_scheduler = ""
+    GHString_sampler = ""
+    GHPrompt_positive = ""
+    GHFile_imagedepth = ""
+    GHFile_imagestyle = ""
+    GHFile_imagesketch = ""
+    GHInteger_batch = ""
+    GHInteger_steps = ""
+    GHInteger_seed = ""
     GHFloat_controlnetsketchstrength = ""
     GHFloat_controlnetdepthstrength = ""
-    GHInteger_seed = ""
-    GHFloat_cfg = ""
-    GHInteger_steps = ""
     GHFloat_denoise = ""
-    GHString_sampler = ""
-    GHString_scheduler = ""
+    GHFloat_cfg = ""
     APIjsonfile = ""
     for i in range(len(args)):
-        if args[i] == "--GHPrompt_positive" and i + 1 < len(args):
-            GHPrompt_positive = args[i + 1]
-        elif args[i] == "--GHFile_imagesketch" and i + 1 < len(args):
-            GHFile_imagesketch = args[i + 1]
-        elif args[i] == "--GHFile_imagedepth" and i + 1 < len(args):
-            GHFile_imagedepth = args[i + 1]
-        elif args[i] == "--GHFile_imagestyle" and i + 1 < len(args):
-            GHFile_imagestyle = args[i + 1]
-        elif args[i] == "--GHInteger_batch" and i + 1 < len(args):
-            GHInteger_batch = args[i + 1]
-        elif args[i] == "--GHString_model" and i + 1 < len(args):
+        if args[i] == "--GHString_model" and i + 1 < len(args):
             GHString_model = args[i + 1]
-        elif args[i] == "--GHString_clipvision" and i + 1 < len(args):
-            GHString_clipvision = args[i + 1]
-        elif args[i] == "--GHString_contolnetsketch" and i + 1 < len(args):
-            GHString_contolnetsketch = args[i + 1]
         elif args[i] == "--GHString_contolnetdepth" and i + 1 < len(args):
             GHString_contolnetdepth = args[i + 1]
         elif args[i] == "--GHString_stylemodel" and i + 1 < len(args):
             GHString_stylemodel = args[i + 1]
+        elif args[i] == "--GHString_contolnetsketch" and i + 1 < len(args):
+            GHString_contolnetsketch = args[i + 1]
+        elif args[i] == "--GHString_clipvision" and i + 1 < len(args):
+            GHString_clipvision = args[i + 1]
+        elif args[i] == "--GHString_scheduler" and i + 1 < len(args):
+            GHString_scheduler = args[i + 1]
+        elif args[i] == "--GHString_sampler" and i + 1 < len(args):
+            GHString_sampler = args[i + 1]
+        elif args[i] == "--GHPrompt_positive" and i + 1 < len(args):
+            GHPrompt_positive = args[i + 1]
+        elif args[i] == "--GHFile_imagedepth" and i + 1 < len(args):
+            GHFile_imagedepth = args[i + 1]
+        elif args[i] == "--GHFile_imagestyle" and i + 1 < len(args):
+            GHFile_imagestyle = args[i + 1]
+        elif args[i] == "--GHFile_imagesketch" and i + 1 < len(args):
+            GHFile_imagesketch = args[i + 1]
+        elif args[i] == "--GHInteger_batch" and i + 1 < len(args):
+            GHInteger_batch = args[i + 1]
+        elif args[i] == "--GHInteger_steps" and i + 1 < len(args):
+            GHInteger_steps = args[i + 1]
+        elif args[i] == "--GHInteger_seed" and i + 1 < len(args):
+            GHInteger_seed = args[i + 1]
         elif args[i] == "--GHFloat_controlnetsketchstrength" and i + 1 < len(args):
             GHFloat_controlnetsketchstrength = args[i + 1]
         elif args[i] == "--GHFloat_controlnetdepthstrength" and i + 1 < len(args):
             GHFloat_controlnetdepthstrength = args[i + 1]
-        elif args[i] == "--GHInteger_seed" and i + 1 < len(args):
-            GHInteger_seed = args[i + 1]
-        elif args[i] == "--GHFloat_cfg" and i + 1 < len(args):
-            GHFloat_cfg = args[i + 1]
-        elif args[i] == "--GHInteger_steps" and i + 1 < len(args):
-            GHInteger_steps = args[i + 1]
         elif args[i] == "--GHFloat_denoise" and i + 1 < len(args):
             GHFloat_denoise = args[i + 1]
-        elif args[i] == "--GHString_sampler" and i + 1 < len(args):
-            GHString_sampler = args[i + 1]
-        elif args[i] == "--GHString_scheduler" and i + 1 < len(args):
-            GHString_scheduler = args[i + 1]
+        elif args[i] == "--GHFloat_cfg" and i + 1 < len(args):
+            GHFloat_cfg = args[i + 1]
         elif args[i] == "--APIjsonfile" and i + 1 < len(args):
             APIjsonfile = args[i + 1]
-    return GHPrompt_positive, GHFile_imagesketch, GHFile_imagedepth, GHFile_imagestyle, GHInteger_batch, GHString_model, GHString_clipvision, GHString_contolnetsketch, GHString_contolnetdepth, GHString_stylemodel, GHFloat_controlnetsketchstrength, GHFloat_controlnetdepthstrength, GHInteger_seed, GHFloat_cfg, GHInteger_steps, GHFloat_denoise, GHString_sampler, GHString_scheduler, APIjsonfile
-GHPrompt_positive, GHFile_imagesketch, GHFile_imagedepth, GHFile_imagestyle, GHInteger_batch, GHString_model, GHString_clipvision, GHString_contolnetsketch, GHString_contolnetdepth, GHString_stylemodel, GHFloat_controlnetsketchstrength, GHFloat_controlnetdepthstrength, GHInteger_seed, GHFloat_cfg, GHInteger_steps, GHFloat_denoise, GHString_sampler, GHString_scheduler, APIjsonfile = parse_arguments(sys.argv[1:])
+    return GHString_model, GHString_contolnetdepth, GHString_stylemodel, GHString_contolnetsketch, GHString_clipvision, GHString_scheduler, GHString_sampler, GHPrompt_positive, GHFile_imagedepth, GHFile_imagestyle, GHFile_imagesketch, GHInteger_batch, GHInteger_steps, GHInteger_seed, GHFloat_controlnetsketchstrength, GHFloat_controlnetdepthstrength, GHFloat_denoise, GHFloat_cfg, APIjsonfile
+GHString_model, GHString_contolnetdepth, GHString_stylemodel, GHString_contolnetsketch, GHString_clipvision, GHString_scheduler, GHString_sampler, GHPrompt_positive, GHFile_imagedepth, GHFile_imagestyle, GHFile_imagesketch, GHInteger_batch, GHInteger_steps, GHInteger_seed, GHFloat_controlnetsketchstrength, GHFloat_controlnetdepthstrength, GHFloat_denoise, GHFloat_cfg, APIjsonfile = parse_arguments(sys.argv[1:])
 
 def queue_prompt(prompt):
     p = {"prompt": prompt}
